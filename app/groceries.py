d6c8d9f8f84453c6011da3903bfe6719bdc41a2b
ishah2001 <30806568+ishah2001@users.noreply.github.com>
2022-03-28 18:00:39 -0400

    Refactor across files

--- app/groceries.py
+++ app/groceries.py
@@ -7,19 +7,7 @@
 
 import os
 
-
-
-
-def to_usd(my_price):
-
-	"""
-	This is a doctstring. It tells us what this function is about.
-
-	Invoke like this: to_usd(9.999)
-
-	"""
-
-	return '${:,.2f}'.format(my_price)
+from app.utils import to_usd
 
 
 
---
+++ app/utils.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+def to_usd(my_price):
+
+	"""
+	This is a doctstring. It tells us what this function is about.
+
+	Invoke like this: to_usd(9.999)
+
+	"""
+
+	return '${:,.2f}'.format(my_price)
